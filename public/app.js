fb71542a361cd24b1a2bb547fc2f7453297b8e96
raghavendraacteion <32730008+raghavendraacteion@users.noreply.github.com>
2019-10-12 18:35:23 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -71,7 +71,50 @@ sampleApp.controller('showslobookpageecontroller', function($scope, $routeParams
 			var listItemsHtml = '';
 			for(var i=0; i < rowss.length; i++)
 			{
-			      listItemsHtml += ('<tr><td style="width:20%" ><div title="">'+rowss[i].name+'</div></td><td style="width:20%" ><div>&nbsp;'+rowss[i].slot_start_time__c+'</div></td><td style="width:24%" ><div title="">'+rowss[i].slot_start_time__c+'</div></td><td style="width:24%" ><div title="">'+rowss[i].slot_start_time__c+'</div></td><td style="width:12%" scope="col"><div title="">'+rowss[i].status__c+'</div></td></tr>');
+			      var schstarttime = new Date(rowss[i].slot_start_time__c);
+			      schstarttime.setHours( schstarttime.getHours() -7 );
+			      var schendtime = new Date(rowss[i].slot_end_time__c);
+			      schendtime.setHours( schendtime.getHours() -7 );
+			      var sttm;
+			      var endtm;
+			      var atslst = schstarttime.split("T");
+			      if(schstarttime.getHours() == 0)
+			      {
+				   sttm = '12 AM';  
+			      }
+			      else if(schstarttime.getHours() < 12)
+			      {
+				   sttm = schstarttime.getHours()+' AM';
+			      }
+			      else if(schstarttime.getHours() == 12)
+			      {
+				    sttm = '12 PM';
+			      }
+			      else
+			      {
+				  var temp = schstarttime.getHours()-12;
+				  sttm = temp+' PM'
+			      }
+			      if(schendtime.getHours() == 0)
+			      {
+				   endtm = '12 AM';  
+			      }
+			      else if(schendtime.getHours() < 12)
+			      {
+				   endtm = schendtime.getHours()+' AM';
+			      }
+			      else if(schendtime.getHours() == 12)
+			      {
+				    endtm = '12 PM';
+			      }
+			      else
+			      {
+				  var temp = schendtime.getHours()-12;
+				  endtm = temp+' PM'
+			      }
+			      var tttme = atslst[0]+' '+sttm+' - '+endtm;
+				
+			      listItemsHtml += ('<tr><td style="width:20%" ><div title="">'+rowss[i].name+'</div></td><td style="width:20%" ><div>&nbsp;'+tttme+'</div></td><td style="width:24%" ><div title="">'+rowss[i].slot_start_time__c+'</div></td><td style="width:24%" ><div title="">'+rowss[i].slot_start_time__c+'</div></td><td style="width:12%" scope="col"><div title="">'+rowss[i].status__c+'</div></td></tr>');
 			}
 			document.querySelector('#sltsdivv').innerHTML = listItemsHtml;
 			//alert($scope.slotss);
