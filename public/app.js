7832f4dbff4c9ba80a95f5184c43b705a28f32cb
raghavendraacteion <32730008+raghavendraacteion@users.noreply.github.com>
2019-10-17 18:52:27 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -62,66 +62,16 @@ sampleApp.controller('showslobookpageecontroller', function($scope, $routeParams
 		contentType: "application/json; charset=utf-8",
 		dataType: "json",
 		success: function(data) {
-			alert(JSON.stringify(data));
-		/*	var rowss = data.rows;
-			$scope.slotss = rowss;
-			$scope.testd = rowss[0].slot_start_time__c;
+			//alert(JSON.stringify(data));
+			var rowss = data;
+			//$scope.slotss = rowss;
+			//$scope.testd = rowss[0].slot_start_time__c;
 			var listItemsHtml = '';
 			for(var i=0; i < rowss.length; i++)
 			{
-			      var schstarttime = new Date(rowss[i].slot_start_time__c);
-			      schstarttime.setHours( schstarttime.getHours() -7 );
-			      var scstrstrng = schstarttime.toUTCString();
-			      var scstrlstt = scstrstrng.split(" ");
-			      var scstrlstrttmelst = scstrlstt[4].split(":");
-			      var schendtime = new Date(rowss[i].slot_end_time__c);
-			      schendtime.setHours( schendtime.getHours() -7 );
-			      var schendstrng = schendtime.toUTCString();
-			      var schendstrnglst = schendstrng.split(" ");
-			      var schdendtmlstt = schendstrnglst[4].split(":");
-			      var sttm;
-			      var endtm;
-			     
-			      if(parseInt(scstrlstrttmelst[0]) == 0)
-			      {
-				   sttm = '12 AM';  
-			      }
-			      else if(parseInt(scstrlstrttmelst[0]) < 12)
-			      {
-				   sttm = scstrlstrttmelst[0]+' AM';
-			      }
-			      else if(parseInt(scstrlstrttmelst[0]) == 12)
-			      {
-				    sttm = '12 PM';
-			      }
-			      else
-			      {
-				  var temp = parseInt(scstrlstrttmelst[0])-12;
-				  sttm = temp+' PM'
-			      }
-				alert(sttm);
-			      if(parseInt(schdendtmlstt[0]) == 0)
-			      {
-				   endtm = '12 AM';  
-			      }
-			      else if(parseInt(schdendtmlstt[0]) < 12)
-			      {
-				   endtm = schdendtmlstt[0]+' AM';
-			      }
-			      else if(parseInt(schdendtmlstt[0]) == 12)
-			      {
-				    endtm = '12 PM';
-			      }
-			      else
-			      {
-				  var temp = parseInt(schdendtmlstt[0])-12;
-				  endtm = temp+' PM'
-			      }
-			      var tttme = scstrlstt[1]+' '+scstrlstt[2]+', '+scstrlstt[3]+' '+sttm+' -- '+endtm;
-				
-			      listItemsHtml += ('<tr><td style="width:20%" ><div title="">'+rowss[i].name+'</div></td><td style="width:20%" ><div>&nbsp;'+tttme+'</div></td><td style="width:24%" ><div title="">'+rowss[i].slot_start_time__c+'</div></td><td style="width:24%" ><div title="">'+rowss[i].slot_start_time__c+'</div></td><td style="width:12%" scope="col"><div title="">'+rowss[i].status__c+'</div></td></tr>');
+			      listItemsHtml += ('<tr><td style="width:20%" ><div title="">'+rowss[i].sltname+'</div></td><td style="width:20%" ><div>&nbsp;'+rowss[i].slttme+'</div></td><td style="width:24%" ><div title="">'+rowss[i].deptname+'</div></td><td style="width:24%" ><div title="">'+rowss[i].subdeptname+'</div></td><td style="width:12%" scope="col"><div title="">'+rowss[i].statuss+'</div></td></tr>');
 			}
-			document.querySelector('#sltsdivv').innerHTML = listItemsHtml;  */
+			document.querySelector('#sltsdivv').innerHTML = listItemsHtml;  
 		},
 		error: function(err) {
 			errorMessage.text(err.responseJSON.error);
