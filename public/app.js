06dcdaf2124f94f4d4440242928010a2e6edd570
raghavendraacteion <32730008+raghavendraacteion@users.noreply.github.com>
2019-10-30 09:59:05 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -413,6 +413,8 @@ sampleApp.controller('calendarpagecontroller11', function($scope, $routeParams) 
 	var endstrng = enddtlst[1]+' '+ enddtlst[2];
 	var dtrangestrng = strtstrng+' to '+endstrng;
 	var ttlstrng = dtrangestrng+', '+wkenddt.getFullYear();
+	document.getElementById("clndrhedngspan").textContent= ttlstrng;
+	document.getElementById("schdlrrangehdngspn").textContent= ttlstrng;
 	alert(ttlstrng);
 	$.ajax({
 		url: "/fetchdepartmentss",
